934afd4fb60874de3eb00cebd2b4334c76da6bed
Arda Moin <109446453+ardamoin@users.noreply.github.com>
2022-07-18 22:41:54 -0400

    Update WordPDFGUI.java

--- src/WordPDFGUI.java
+++ src/WordPDFGUI.java
@@ -1,12 +1,9 @@
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
 import java.awt.dnd.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
-import java.util.List;
 
 
 import com.spire.doc.Document;
@@ -31,7 +28,7 @@ public class WordPDFGUI extends JFrame {
         this.setContentPane(mainPanel);
 
 
-        MyDragDropListener myDragDropListener = new MyDragDropListener();
+        MyDragDropListener myDragDropListener = new MyDragDropListener(wordFilePath);
         new DropTarget(wordFilePath, myDragDropListener);
 
         wordFilePath.setEditable(false);
@@ -99,71 +96,6 @@ public class WordPDFGUI extends JFrame {
         frame.setSize(650,450);
         frame.setVisible(true);
 
-    }
-
-    public class MyDragDropListener implements DropTargetListener {
-
-        @Override
-        public void drop(DropTargetDropEvent event) {
-
-            // Accept copy drops
-            event.acceptDrop(DnDConstants.ACTION_COPY);
-
-            // Get the transfer which can provide the dropped item data
-            Transferable transferable = event.getTransferable();
-
-            // Get the data formats of the dropped item
-            DataFlavor[] flavors = transferable.getTransferDataFlavors();
-
-            // Loop through the flavors
-            for (DataFlavor flavor : flavors) {
-
-                try {
-
-                    // If the drop items are files
-                    if (flavor.isFlavorJavaFileListType()) {
-
-                        // Get all of the dropped files
-                        List<File> files = (List) transferable.getTransferData(flavor);
-
-                        // Loop them through
-                        for (File file : files) {
-
-                            targetFile = file;
-                            wordFilePath.setText(file.getPath());
-
-                        }
-
-                    }
-
-                } catch (Exception e) {
-
-                    // Print out the error stack
-                    e.printStackTrace();
-
-                }
-            }
-
-            // Inform that the drop is complete
-            event.dropComplete(true);
-
-        }
-
-        @Override
-        public void dragEnter(DropTargetDragEvent event) {
-        }
-
-        @Override
-        public void dragExit(DropTargetEvent event) {
-        }
-
-        @Override
-        public void dragOver(DropTargetDragEvent event) {
-        }
-
-        @Override
-        public void dropActionChanged(DropTargetDragEvent event) {
-        }
     }
 
     public static void convertToPdf(File file) throws FileNotFoundException {
